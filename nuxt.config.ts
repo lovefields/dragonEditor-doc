7da86fe4a72b4d91aeeed5df4e3fb2490129d05b
lovefield <hho1993@gmail.com>
2024-08-29 09:23:28 +0900

    chore: add module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
 
     css: ["@/assets/scss/common.scss"],
 
-    modules: ["dragon-editor"],
+    modules: ["@pinia/nuxt", "dragon-editor"],
 
     vite: {
         css: {
